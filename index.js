2d3f9d60b6592ab87313ca7644e5766b808d7690
Blue Mouse <durexp@protonmail.ch>
2021-10-21 23:15:03 +0300

    fixed build issue

--- index.js
+++
@@ -1,8 +0,0 @@
-import Alert from './src/Alert'
-import actions from './src/redux/actions'
-import alertReducer from './src/redux/reducer'
-import { types, transitions } from './src/redux/constants'
-
-export {
-  Alert, actions, alertReducer, types, transitions,
-}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import Alert from './Alert'
+import actions from './redux/actions'
+import alertReducer from './redux/reducer'
+import { types, transitions } from './redux/constants'
+
+export {
+  Alert, actions, alertReducer, types, transitions,
+}
